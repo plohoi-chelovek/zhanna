288afd58a33f25863df461d9a45d9ffd9dc93b63
Ast <plohoi.chelovek@gmail.com>
2013-04-28 00:30:47 +0300

    prototype of watching subsystem

---
+++ src/main/java/ua/com/zhanna/watching/Watching.java
@@ -0,0 +1,30 @@
+package ua.com.zhanna.watching;
+
+import java.nio.file.*;
+import static java.nio.file.StandardWatchEventKinds.*;
+import static java.nio.file.LinkOption.*;
+import java.nio.file.attribute.*;
+import java.io.*;
+import java.util.*;
+
+class Watching {
+    public Watching(String dir) throws IOException {
+        // this.watcher = FileSystems.getDefault().newWatchService();
+        // this.keys = new HashMap<WatchKey,Path>();
+        // this.recursive = recursive;
+
+        // if (recursive) {
+        //     System.out.format("Scanning %s ...\n", dir);
+        //     registerAll(dir);
+        //     System.out.println("Done.");
+        // } else {
+        //     register(dir);
+        // }
+    }
+
+    public void start() {
+	//out in the city
+    }
+}    
+
+
