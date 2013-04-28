c9686f3bef0fa06603844d8b24eb70185c733f39
Ast <plohoi.chelovek@gmail.com>
2013-04-29 00:06:18 +0300

    finish working on printing subsystem

--- src/main/java/ua/com/zhanna/printing/Printing.java
+++ src/main/java/ua/com/zhanna/printing/Printing.java
@@ -8,21 +8,36 @@ import java.nio.charset.*;
 import java.awt.print.*;
 
 import javax.swing.*;
+import javax.swing.text.*;
+
+import org.pmw.tinylog.Logger;
 
 class Printing {
-    public void print(Path path) {
-	JTextArea textArea = new JTextArea();
-	try (BufferedReader reader = Files.newBufferedReader(path, Charset.defaultCharset())) {
-		    String line = null;
-		    while ((line = reader.readLine()) != null) {
-			textArea.append(line + "\n");
-		    }
-		    textArea.print();
-	    } catch (Exception e) { /* log some where */ }
+    public void print(final Path path) {
+	SwingUtilities.invokeLater(new Runnable() {
+		public void run() {
+		    JTextArea textArea = new JTextArea();
+		    try (BufferedReader reader = 
+			 Files.newBufferedReader(path, Charset.defaultCharset())) {
+			    String line = null;
+			    while ((line = reader.readLine()) != null) {
+				textArea.append(line + System.lineSeparator());
+			    }
+			    textArea.print();
+			} catch (Exception e) { Logger.error(e.getMessage()); }
+		}
+	    });
     }
 
     /* TEST IT */
     public static void main(String[] args) {
-	//soon
+	Printing printing = new Printing();
+	printing.print(Paths.get(args[0]));
+	printing.print(Paths.get(args[0]));
+	System.out.println("exit from main");
     }
 }
+
+
+
+
