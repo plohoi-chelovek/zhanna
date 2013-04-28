7228495d0956149e654edef308bad250ccf0ca12
Ast <plohoi.chelovek@gmail.com>
2013-04-29 01:09:38 +0300

    finish the job?

--- src/main/java/ua/com/zhanna/Application.java
+++ src/main/java/ua/com/zhanna/Application.java
@@ -1,7 +1,41 @@
 package ua.com.zhanna;
 
+import java.io.*;
+
+import org.pmw.tinylog.Logger;
+
+import ua.com.zhanna.watching.*;
+import ua.com.zhanna.operation.*;
+import ua.com.zhanna.printing.*;
+
 public class Application {
-    public static void main(String[] args) {
-	System.out.println("test");
+    private Watching watching;
+    private Operation operation;
+    private Printing printing;
+
+    public Application(String path) throws IOException {
+	watching = new Watching(path);
+	operation = new EncodingOperation();
+	printing = new Printing();
+    }
+
+    public void start() {
+	watching.addWatchingListener(new WatchingListener() {
+		public void entryIsCreated(EntryEvent e) {
+		    printing.print(operation.operate(e.getEntry()));
+		}
+		public void interruptedError(ErrorEvent event) {
+		    Logger.error("interrupted error");
+		}
+		public void watchKeyError(ErrorEvent event) {
+		    Logger.error("watchkey error");
+		}
+	    });
+	watching.start();
+    }
+
+    /* ENTER POINT */
+    public static void main(String[] args) throws IOException {
+	new Application(args[0]).start();
     }
 }
