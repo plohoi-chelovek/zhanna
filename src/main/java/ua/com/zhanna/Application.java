b755ce5a88579177135b40a5ac468a68f361912a
Ast <plohoi.chelovek@gmail.com>
2013-04-29 01:56:29 +0300

    help file

--- src/main/java/ua/com/zhanna/Application.java
+++ src/main/java/ua/com/zhanna/Application.java
@@ -1,6 +1,7 @@
 package ua.com.zhanna;
 
 import java.io.*;
+import java.nio.charset.*;
 
 import org.pmw.tinylog.Logger;
 
@@ -9,14 +10,21 @@ import ua.com.zhanna.operation.*;
 import ua.com.zhanna.printing.*;
 
 public class Application {
-    private Watching watching;
+    private static final String HELP_FILE = "/usage.txt";
+    private static final String HELP_FILE_ENCODING = "UTF-8";
+
     private Operation operation;
+    private Watching watching;
     private Printing printing;
 
-    public Application(String path) throws IOException {
+    public Application(String encoding, String path) throws IOException {
+	operation = new EncodingOperation(encoding);
 	watching = new Watching(path);
-	operation = new EncodingOperation();
 	printing = new Printing();
+    }
+
+    public Application() throws IOException {
+	this(Charset.defaultCharset().toString(), ".");
     }
 
     public void start() {
@@ -34,8 +42,28 @@ public class Application {
 	watching.start();
     }
 
+    public void usage() {
+	try {
+	    BufferedReader in = 
+		new BufferedReader(new InputStreamReader(getClass().getResourceAsStream(HELP_FILE),
+							 Charset.forName(HELP_FILE_ENCODING)));
+	    String s;
+	    while ((s = in.readLine()) != null)
+		System.out.println(s);
+	} catch (Exception e) {
+	    Logger.error("Ooops: " + e.getMessage());
+	}
+    }
+
     /* ENTER POINT */
     public static void main(String[] args) throws IOException {
-	new Application(args[0]).start();
+	if (args.length != 2 || args[0].equals("--help"))
+	    new Application().usage();
+	else
+	    new Application(args[0], args[1]).start();
     }
 }
+
+
+
+
