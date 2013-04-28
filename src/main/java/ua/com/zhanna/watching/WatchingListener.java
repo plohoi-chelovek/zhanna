c70ed7eed908f61b05097550de645f37acdf55c2
Ast <plohoi.chelovek@gmail.com>
2013-04-28 13:14:18 +0300

    some bug fixes i guess

--- src/main/java/ua/com/zhanna/watching/WatchingListener.java
+++ src/main/java/ua/com/zhanna/watching/WatchingListener.java
@@ -4,4 +4,6 @@ import java.util.*;
 
 public interface WatchingListener extends EventListener {
     public void entryIsCreated(EntryEvent event);
+    public void interruptedError(ErrorEvent event);
+    public void watchKeyError(ErrorEvent event);
 }
